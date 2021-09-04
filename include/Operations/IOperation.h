51dec7dfd176a2573b9309088b782c46b4452cc9
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2021-09-03 23:15:33 -0500

    add index_sequence implementation for C++11

--- include/Operations/IOperation.h
+++ include/Operations/IOperation.h
@@ -6,6 +6,36 @@
 #define IOPERATION_H
 namespace OperationArchitecture
 {
+#if __cplusplus > 201103L
+	template<std::size_t... Ints>
+	using index_sequence = std::index_sequence<Ints...>;
+
+	template<typename... T>
+	using index_sequence_for = std::index_sequence_for<T...>;
+#else
+	template<typename T, T... Ints>
+	struct integer_sequence
+	{
+		typedef T value_type;
+		static constexpr std::size_t size() { return sizeof...(Ints); }
+	};
+	
+	template<std::size_t... Ints>
+	using index_sequence = integer_sequence<std::size_t, Ints...>;
+	
+	template<typename T, std::size_t N, T... Is>
+	struct make_integer_sequence : make_integer_sequence<T, N-1, N-1, Is...> {};
+	
+	template<typename T, T... Is>
+	struct make_integer_sequence<T, 0, Is...> : integer_sequence<T, Is...> {};
+	
+	template<std::size_t N>
+	using make_index_sequence = make_integer_sequence<std::size_t, N>;
+	
+	template<typename... T>
+	using index_sequence_for = make_index_sequence<sizeof...(T)>;
+#endif
+
     template<typename RET, typename... PARAMS>
     class IOperation : public IOperationBase
     {
@@ -14,12 +44,12 @@ namespace OperationArchitecture
         virtual RET Execute(PARAMS...) = 0;
         void AbstractExecute(Variable **variables) override
         {
-            std::index_sequence_for<PARAMS...> paramIndex = std::index_sequence_for<PARAMS...>{};
+            index_sequence_for<PARAMS...> paramIndex = index_sequence_for<PARAMS...>{};
             AbstractExecuteImpl(paramIndex, variables);
         }
 
         template<std::size_t... ParamIndex>
-        void AbstractExecuteImpl(std::index_sequence<ParamIndex...>, Variable **variables) {
+        void AbstractExecuteImpl(index_sequence<ParamIndex...>, Variable **variables) {
             variables[0]->Set(Execute(variables[ParamIndex + 1]->To<PARAMS>()...));
         }
     };
@@ -32,12 +62,12 @@ namespace OperationArchitecture
         virtual void Execute(PARAMS...) = 0;
         void AbstractExecute(Variable **variables) override
         {
-            std::index_sequence_for<PARAMS...> paramIndex = std::index_sequence_for<PARAMS...>{};
+            index_sequence_for<PARAMS...> paramIndex = index_sequence_for<PARAMS...>{};
             AbstractExecuteImpl(paramIndex, variables);
         }
 
         template<std::size_t... ParamIndex>
-        void AbstractExecuteImpl(std::index_sequence<ParamIndex...>, Variable **variables) {
+        void AbstractExecuteImpl(index_sequence<ParamIndex...>, Variable **variables) {
             Execute(variables[ParamIndex]->To<PARAMS>()...);
         }
     };
@@ -50,13 +80,13 @@ namespace OperationArchitecture
         virtual std::tuple<RETs...> Execute(PARAMS...) = 0;
         void AbstractExecute(Variable **variables) override
         {
-            std::index_sequence_for<RETs...> retIndex = std::index_sequence_for<RETs...>{};
-            std::index_sequence_for<PARAMS...> paramIndex = std::index_sequence_for<PARAMS...>{};
+            index_sequence_for<RETs...> retIndex = index_sequence_for<RETs...>{};
+            index_sequence_for<PARAMS...> paramIndex = index_sequence_for<PARAMS...>{};
             AbstractExecuteImpl(retIndex, paramIndex, variables);
         }
 
         template<std::size_t... RetIndex, std::size_t... ParamIndex>
-        void AbstractExecuteImpl(std::index_sequence<RetIndex...>, std::index_sequence<ParamIndex...>, Variable **variables) {
+        void AbstractExecuteImpl(index_sequence<RetIndex...>, index_sequence<ParamIndex...>, Variable **variables) {
             std::tuple<RETs...> rets = Execute(variables[ParamIndex + NumberOfReturnVariables]->template To<PARAMS>()...);
             //this is fucking stupid, why can't i just "variables[RetIndex]->Set(std::get<RetIndex>(rets)...;" ?
             int expand_type[] = { (variables[RetIndex]->Set(std::get<RetIndex>(rets)), 0)... };
