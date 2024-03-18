6ad2ad0391cf50e5926d9ba325e8075f4b4cd585
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2024-03-18 11:57:36 -0500

    add parameters to MockOperation

--- mock/include/Operations/MockOperation.h
+++ mock/include/Operations/MockOperation.h
@@ -4,13 +4,24 @@
 #define MOCKOPERATION_H
 namespace OperationArchitecture
 {
-	template<typename return_t>
-	class MockOperation : public Operation<return_t>
+	template<typename return_t, typename... PARAMS>
+	class MockOperation : public Operation<return_t, PARAMS...>
 	{
 	public:
 		return_t ReturnValue;
-		return_t Execute() override {
+		std::tuple<PARAMS...> Parameters;
+		return_t Execute(PARAMS... parameters) override {
+			Parameters = std::tuple<PARAMS...>(parameters...);
 			return ReturnValue;
+		}
+	};
+	template<typename... PARAMS>
+	class MockOperation<void, PARAMS...> : public Operation<void, PARAMS...>
+	{
+	public:
+		std::tuple<PARAMS...> Parameters;
+		void Execute(PARAMS... parameters) override {
+			Parameters = std::tuple<PARAMS...>(parameters...);
 		}
 	};
 }
